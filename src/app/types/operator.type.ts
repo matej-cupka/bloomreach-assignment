bc854c0e963c81b88907da3044139202b4ba10e9
Matej Čupka <matocupka@me.com>
2023-03-02 12:03:00 +0100

    Refactor of operator.type.ts

--- src/app/types/operator.type.ts
+++ src/app/types/operator.type.ts
@@ -1,3 +1,6 @@
-export type TOperatorString = 'equals' | 'does not equal' | 'contains' | 'does not contain';
-export type TOperatorNumber = 'equal to' | 'in between' | 'less than' | 'greater than';
+export const OPERATOR_STRING_ARR = ['equals', 'does not equal', 'contains', 'does not contain'] as const;
+export const OPERATOR_NUMBER_ARR = ['equal to', 'in between', 'less than', 'greater than'] as const;
+
+export type TOperatorString = typeof OPERATOR_STRING_ARR[number];
+export type TOperatorNumber = typeof OPERATOR_NUMBER_ARR[number];
 export type TOperator = TOperatorString | TOperatorNumber;
